26f30b97c50384c7b8cda9f49f5aaf5f0376084c
Alexander Moreno <alexmore080@gmail.com>
2025-11-11 19:19:08 -0600

    Suicide Monkey

---
+++ Assets/Script/Enemy/EnemyTypes/BombMonkey/BombMonkey.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BombMonkey : Enemy
+{
+    [Header("Bomb Monkey Stats")]
+    [SerializeField] private float detectionRange = 15f;
+    [SerializeField] private float wanderingMoveSpeed = 5f;
+    [SerializeField] private float chasingMoveSpeed = 10f;
+    [SerializeField] private int maxHealth = 50;
+
+    private BombMonkeyExploder exploder;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        enemyHealth.StartingHealth = maxHealth;
+        exploder = GetComponent<BombMonkeyExploder>();
+        if (exploder == null) exploder = gameObject.AddComponent<BombMonkeyExploder>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        SetState(EnemyStateMachine.EnemyState.Idle);
+
+        if (enemyDetection != null) enemyDetection.DetectionRange = detectionRange;
+        if (enemyMovement != null) enemyMovement.DefaultMoveSpeed = wanderingMoveSpeed;
+        if (enemyChasing != null) enemyChasing.ChaseSpeed = chasingMoveSpeed;
+        if (enemyHealth != null) enemyHealth.StartingHealth = maxHealth;
+
+        if (enemyAttacking != null)
+            enemyAttacking.OnPlayerEnteredRange += HandleDetonateOnProximity;
+
+        if (stateMachine != null)
+            stateMachine.OnStateChanged += HandleStateChangedForExplosion;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        if (enemyAttacking != null)
+            enemyAttacking.OnPlayerEnteredRange -= HandleDetonateOnProximity;
+        if (stateMachine != null)
+            stateMachine.OnStateChanged -= HandleStateChangedForExplosion;
+    }
+
+    private void HandleDetonateOnProximity()
+    {
+        if (exploder != null && !exploder.HasExploded)
+            exploder.TriggerExplosion(selfDetonated: true);
+    }
+
+    private void HandleStateChangedForExplosion(EnemyStateMachine.EnemyState oldState, EnemyStateMachine.EnemyState newState)
+    {
+        if (newState == EnemyStateMachine.EnemyState.Dead && exploder != null && !exploder.HasExploded)
+            exploder.TriggerExplosion(selfDetonated: false);
+    }
+}
