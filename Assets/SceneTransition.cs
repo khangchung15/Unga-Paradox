463f65ccccf64c75dca674533c13a8a8ac7be975
KvvngE <eddiefrenney@gmail.com>
2025-12-02 20:18:10 -0600

    BRuh

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -4,11 +4,12 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
-    public string entryPointName;   // name of spawn point in the next scene
+    public string entryPointName;  // Name of spawn object in the next scene
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        // Check the ROOT object’s tag (handles child colliders)
+        if (other.transform.root.CompareTag("Player"))
         {
             SceneEntryManager.lastEntryPoint = entryPointName;
             SceneManager.LoadScene(sceneToLoad);
